5446410b96a3a0645988ab3bb3e7f9d76f1af1dc
Shea Fitzgerald <sheamusfitz@gmail.com>
2019-03-15 23:29:15 -0400

    done
    submitted

--- Project2_Fitzgerald.py
+++ Project2_Fitzgerald.py
@@ -3,7 +3,8 @@ Project 2 "Chaos Balls"
 '''
 
 import numpy as np
-import matplotlib.pyplot as plyt
+import matplotlib.pyplot as plt
+import colorsys
 
 #	#	#	#	#	#	#	#	#	#	#
 #		INITIAL CONDITIONS HERE			#
@@ -17,14 +18,14 @@ xb0_raw = 3
 va0_raw = 0
 vb0_raw = 0
 
-n_cols=10000 #the number of collisions to calculate
+n_cols=20000 #the number of collisions to calculate
 
 #Exact calculations: timesteps
 dt = 0.1
-part2steps = 4000
+part2steps = 100000
 
 #higher values of tau give fewer points in the autocorrelation function
-tau = 5
+tau = 1
 
 
 #										#
@@ -38,8 +39,6 @@ tau = 5
 g_raw=9.81
 e_raw=.5*ma_raw*va0_raw**2+.5*mb_raw*vb0_raw**2+(ma_raw*xa0_raw+mb_raw*xb0_raw)*g_raw
 #print(f'2-total energy e_raw = {e_raw}')
-
-
 
 def normalize(ma_raw,mb_raw,xa0_raw,xb0_raw,va0_raw,vb0_raw):
 	if xa0_raw>xb0_raw:
@@ -97,7 +96,7 @@ vbgg=np.zeros(part2steps)
 def part1():
 	global tgg,xagg,xbgg,vagg,vbgg
 	#for some reason, THIS ONE needs to be declared global, but none of the others. go figure.
-
+	
 	t_c = 0
 
 	#	normalizing initial conditions
@@ -215,18 +214,18 @@ def part1():
 
 	#poincare section==> psec
 
-	# plyt.figure()
-	# plyt.plot(xb*x_n,vb*v_n,'k.', markersize=1)
-	# #plyt.plot(xb,vb,'k.', markersize=1)
-	# plyt.show()
+	# plt.figure()
+	# plt.plot(xb*x_n,vb*v_n,'k.', markersize=1)
+	# #plt.plot(xb,vb,'k.', markersize=1)
+	# plt.show()
 
 # part1()
-# plyt.figure()
-# plyt.plot(xb*x_n,vb*v_n,'k.', markersize=1)
-# plyt.xlabel('$x_b$ (m)')
-# plyt.ylabel('$v_b$ (m/s)')
-# #plyt.plot(xb,vb,'k.', markersize=1)
-# plyt.show()
+# plt.figure()
+# plt.plot(xb*x_n,vb*v_n,'k.', markersize=1)
+# plt.xlabel('$x_b$ (m)')
+# plt.ylabel('$v_b$ (m/s)')
+# #plt.plot(xb,vb,'k.', markersize=1)
+# plt.show()
 
 
 
@@ -313,11 +312,11 @@ def part2():
 		t_ex[n]=n*dt
 		#print(t_ex[n])
 	'''
-	plyt.figure()
-	plyt.plot(t_ex,xa_ex,t_ex,xb_ex,markersize=1)
-	#plyt.plot(t_ex,xa_ex,'.',t_ex,xb_ex,'.',markersize=1)
+	plt.figure()
+	plt.plot(t_ex,xa_ex,t_ex,xb_ex,markersize=1)
+	#plt.plot(t_ex,xa_ex,'.',t_ex,xb_ex,'.',markersize=1)
 	#Axes.set_xlim(right=100)
-	plyt.show()
+	plt.show()
 	print(xb_ex)
 	'''
 
@@ -332,16 +331,16 @@ xb_cor= np.zeros(j_max)
 
 
 
-# plyt.figure()
-# plyt.subplot(111)
-# #	plyt.plot(t_ex,xa_ex,t_ex,xb_ex,markersize=1)
-# plyt.plot(t_ex*t_n,xa_ex*x_n,'.',t_ex*t_n,xb_ex*x_n,'.',markersize=1)
-# plyt.xlabel('$t$ (s)')
-# plyt.ylabel('$x$ (m)')
-# plyt.legend(['$x_a$','$x_b$'],loc=0,markerscale=7)
+# plt.figure()
+# plt.subplot(111)
+# #	plt.plot(t_ex,xa_ex,t_ex,xb_ex,markersize=1)
+# plt.plot(t_ex*t_n,xa_ex*x_n,'.',t_ex*t_n,xb_ex*x_n,'.',markersize=1)
+# plt.xlabel('$t$ (s)')
+# plt.ylabel('$x$ (m)')
+# plt.legend(['$x_a$','$x_b$'],loc=0,markerscale=7)
 # #Axes.set_xlim(right=100)
 # #	print(xb_ex)
-# plyt.show()
+# plt.show()
 
 
 
@@ -395,51 +394,51 @@ def autocor():
 # part2()
 # autocor()
 
-# plyt.figure()
-# plyt.subplot(311)
-# plyt.title(mb_raw)
-# plyt.plot(xb,vb,'k.', markersize=1)
-# plyt.subplot(312)
-# #	plyt.plot(t_ex,xa_ex,t_ex,xb_ex,markersize=1)
-# plyt.plot(t_ex,xa_ex,'.',t_ex,xb_ex,'.',markersize=1)
+# plt.figure()
+# plt.subplot(311)
+# plt.title(mb_raw)
+# plt.plot(xb,vb,'k.', markersize=1)
+# plt.subplot(312)
+# #	plt.plot(t_ex,xa_ex,t_ex,xb_ex,markersize=1)
+# plt.plot(t_ex,xa_ex,'.',t_ex,xb_ex,'.',markersize=1)
 # #	print(xb_ex)
 '''
-plyt.subplot(311)
-plyt.plot(t_cor*t_n,xa_cor*x_n,t_cor*t_n,xb_cor*x_n,markersize=1)
-plyt.xlim(left = -3)
-plyt.legend(['$x_a$','$x_b$'],loc=0)
-plyt.ylabel('$x$ (m)')
+plt.subplot(311)
+plt.plot(t_cor*t_n,xa_cor*x_n,t_cor*t_n,xb_cor*x_n,markersize=1)
+plt.xlim(left = -3)
+plt.legend(['$x_a$','$x_b$'],loc=0)
+plt.ylabel('$x$ (m)')
 
-plyt.subplot(312)
-plyt.plot(t_cor*t_n,xb_cor*x_n,'C1')
-plyt.xlim(right = 20,left = -.5)
-plyt.ylabel('$x$ (m)')
-plyt.legend(['$x_b$'],loc=0)
+plt.subplot(312)
+plt.plot(t_cor*t_n,xb_cor*x_n,'C1')
+plt.xlim(right = 20,left = -.5)
+plt.ylabel('$x$ (m)')
+plt.legend(['$x_b$'],loc=0)
 
-plyt.subplot(313)
-plyt.plot(t_cor*t_n,xa_cor*x_n,'C0')
-plyt.xlim(right = 20,left = -.5)
-plyt.xlabel('$\\tau$ (s)')
-plyt.ylabel('$x$ (m)')
-plyt.legend(['$x_a$'],loc=0)
-plyt.subplots_adjust(hspace=0.2)
-#	plyt.plot(t_cor,xa_cor,t_cor,xb_cor)
-plyt.show()
+plt.subplot(313)
+plt.plot(t_cor*t_n,xa_cor*x_n,'C0')
+plt.xlim(right = 20,left = -.5)
+plt.xlabel('$\\tau$ (s)')
+plt.ylabel('$x$ (m)')
+plt.legend(['$x_a$'],loc=0)
+plt.subplots_adjust(hspace=0.2)
+#	plt.plot(t_cor,xa_cor,t_cor,xb_cor)
+plt.show()
 '''
 ########################
-# plyt.subplot(211)
-# plyt.plot(t_cor*t_n,xa_cor*x_n,'.',t_cor*t_n,xb_cor*x_n,'.',markersize=1)
-# plyt.ylabel('$x$ (m)')
-# plyt.legend(['$x_a$','$x_b$'],loc=0,markerscale=7)
+# plt.subplot(211)
+# plt.plot(t_cor*t_n,xa_cor*x_n,'.',t_cor*t_n,xb_cor*x_n,'.',markersize=1)
+# plt.ylabel('$x$ (m)')
+# plt.legend(['$x_a$','$x_b$'],loc=0,markerscale=7)
 
-# plyt.subplot(212)
-# plyt.plot(t_cor*t_n,xa_cor*x_n,t_cor*t_n,xb_cor*x_n,markersize=1)
-# plyt.xlabel('$\\tau$ (s)')
-# plyt.ylabel('$x$ (m)')
-# plyt.xlim(left = -1, right=30)
-# plyt.legend(['$x_a$','$x_b$'],loc=0)
-# #	plyt.plot(t_cor,xa_cor,t_cor,xb_cor)
-# plyt.show()
+# plt.subplot(212)
+# plt.plot(t_cor*t_n,xa_cor*x_n,t_cor*t_n,xb_cor*x_n,markersize=1)
+# plt.xlabel('$\\tau$ (s)')
+# plt.ylabel('$x$ (m)')
+# plt.xlim(left = -1, right=30)
+# plt.legend(['$x_a$','$x_b$'],loc=0)
+# #	plt.plot(t_cor,xa_cor,t_cor,xb_cor)
+# plt.show()
 
 
 
@@ -468,27 +467,27 @@ part1()
 part2()
 autocor()
 
-plyt.figure()
+plt.figure()
 
-plyt.subplot(311)
-plyt.plot(xb*x_n,vb*v_n,'k.', markersize=1)
-plyt.xlabel('$x_b$ (m)')
-plyt.ylabel('$v_b$ (m/s)')
+plt.subplot(311)
+plt.plot(xb*x_n,vb*v_n,'k.', markersize=1)
+plt.xlabel('$x_b$ (m)')
+plt.ylabel('$v_b$ (m/s)')
 
-plyt.subplot(312)
-plyt.plot(t_ex*t_n,xa_ex*x_n,'.',t_ex*t_n,xb_ex*x_n,'.',markersize=1)
-plyt.legend(['$x_a$','$x_b$'],loc=0,markerscale=7)
-plyt.xlabel('$t$ (s)')
-plyt.ylabel('$x$ (m)')
+plt.subplot(312)
+plt.plot(t_ex*t_n,xa_ex*x_n,'.',t_ex*t_n,xb_ex*x_n,'.',markersize=1)
+plt.legend(['$x_a$','$x_b$'],loc=0,markerscale=7)
+plt.xlabel('$t$ (s)')
+plt.ylabel('$x$ (m)')
 
-plyt.subplot(313)
-plyt.plot(t_cor*t_n,xa_cor*x_n,'.',t_cor*t_n,xb_cor*x_n,'.',markersize=1)
-plyt.legend(['$x_a$','$x_b$'],loc=0,markerscale=7)
-plyt.xlabel('$\\tau$ (s)')
-plyt.ylabel('$x$ (m)')
+plt.subplot(313)
+plt.plot(t_cor*t_n,xa_cor*x_n,'.',t_cor*t_n,xb_cor*x_n,'.',markersize=1)
+plt.legend(['$x_a$','$x_b$'],loc=0,markerscale=7)
+plt.xlabel('$\\tau$ (s)')
+plt.ylabel('$x$ (m)')
 
-plyt.subplots_adjust(hspace=0.5)
-plyt.show()
+plt.subplots_adjust(hspace=0.5)
+plt.show()
 '''
 
 
@@ -513,29 +512,34 @@ part2steps = 4000
 tau = 5
 
 
-'''
-plyt.figure()
+
+plt.figure()
 
 some_list = []
 
 some_step = 1.2
 n = 12
+
+normalize(ma_raw,mb_raw,xa0_raw,xb0_raw,va0_raw,vb0_raw)
+x_n5 = x_n
+v_n5 = v_n
 for i in range(n):
 	some_color = i/n
 	#print(i)
 	part1()
-	ired   = 1-i/n
-	igreen = 0
-	iblue  = (1-ired) #.4*np.cos(3.14*(i+4)/12)+.5
-	plyt.plot(xb,vb,'.', markersize=1,c=[ired, igreen, iblue])
-	some_list.append(va0_raw)
+	hue = i/16
+	plt.plot(xb*x_n5,vb*v_n5,'.', markersize=1,c=colorsys.hsv_to_rgb(hue, 1, 1))
+	some_list.append(np.around(va0_raw,1))
 	va0_raw += some_step
 	print(i,'::   ',va0_raw)
 
-plyt.legend(some_list,loc=0,markerscale=7)
-plyt.xlim(right = 1)
-plyt.show()
-
+plt.legend(some_list,loc=0,markerscale=7,title = '$v_{a,0}$ (m/s)',ncol=3,columnspacing=0.5)
+#plt.xlim(right =1)
+#plt.ylim(bottom = -1.7)
+plt.xlabel('$x_b$ (m)')
+plt.ylabel('$v_b$ (m/s)')
+plt.show()
+'''
 #
 #
 #
@@ -547,7 +551,6 @@ plyt.show()
 #question 7
 #
 #
-'''
 ma_raw = 1.0
 mb_raw = 9
 xa0_raw = 1
@@ -555,10 +558,12 @@ xb0_raw = 3
 vb0_raw = 0
 
 #stable
-va0_raw = 14.4
+#va0_raw = 13.2
 
 #chaotic
-#va0_raw = 0
+va0_raw = 0
+x = np.linspace(0.0,400,400)
+y = 10**(-8)*(np.exp(1*x))
 
 part1()
 part2()
@@ -576,15 +581,42 @@ xb_ex_1 = xb_ex
 
 delta_xb = np.fabs(xb_ex_1-xb_ex_0)
 
-plyt.figure()
-plyt.subplot(121)
-plyt.plot(t_ex,xb_ex_1,'r+',t_ex,xb_ex_0,'bx', markersize=3)
-plyt.subplot(122)
-plyt.plot(t_ex,delta_xb,'.', markersize=1)
-plyt.ylim(bottom = 2.5*10**-7)
-plyt.yscale('log')
-plyt.show()
-'''
+
+
+plt.figure()
+
+plt.subplot(311)
+plt.plot(xb*x_n,vb*v_n,'k.', markersize=1)
+plt.title('Poincaré Section')
+plt.xlabel('$x_b$ (m)')
+plt.ylabel('$v_b$ (m/s)')
+plt.xlim(left=0,right=3)
+plt.ylim(top = 8,bottom = -8)
+
+plt.subplot(312)
+plt.title('Height of particle $b$')
+plt.plot(t_ex*t_n,xb_ex_1*x_n,'r',t_ex*t_n,xb_ex_0*x_n,'k:',markersize=3)
+#plt.plot(t_ex*t_n,xa_ex*x_n,',')
+plt.xlabel('$t$ (s)')
+plt.ylabel('$x_b$ (m)')
+plt.legend(['$x_{a,0}=1$','$\\tilde{x}_{a,0}=1.000001$'],loc=3)
+plt.xlim(left=4500,right=4525)
+
+
+plt.subplot(313)
+plt.plot(t_ex*t_n,delta_xb*x_n,'.', markersize=1)
+plt.ylim(bottom = 10**-8)
+#plt.xlim(left=-1,right=25)
+#plt.yscale('log')
+plt.title('Lyapunov Exponent')
+plt.plot(x,y)
+plt.xlabel('$t$ (s)')
+plt.ylabel('$x$ (m)')
+plt.legend(['$x_b(t)-\\tilde{x}_b(t)$','$10^{-8}e^{1x}$'],markerscale=7,loc=4)
+plt.subplots_adjust(hspace=0.7)
+plt.tight_layout()
+plt.show()
+
 #
 #
 #
